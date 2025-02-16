05e6848c1cc7f1eedaaca9ff54188c01a68df2f8
ductm2205 <imoutstagram@gmail.com>
2025-02-16 16:33:45 +0700

    update: create, update and delete employee

--- src/main/java/se2/tut02/controller/EmployeeController.java
+++ src/main/java/se2/tut02/controller/EmployeeController.java
@@ -6,7 +6,9 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import se2.tut02.model.Employee;
@@ -37,6 +39,35 @@ public class EmployeeController {
         }
 
         return "employeeDetails";
+    }
+
+    @GetMapping(value = "/update/{id}")
+    public String updateEmployee(@PathVariable(value = "id") Long id, Model model) {
+        Employee employee = employeeRepository.findById(id).get();
+        model.addAttribute("employee", employee);
+        return "employeeUpdate";
+    }
+
+    @PostMapping(value = "/save")
+    public String saveUpdate(Employee employee) {
+        employeeRepository.save(employee);
+        return "redirect:/detail/" + employee.getId();
+    }
+
+    @GetMapping(value = "/add")
+    public String addEmployee(Model model) {
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "employeeAdd";
+    }
+
+    @GetMapping(value = "/delete/{id}")
+    public String deleteEmployee(@PathVariable(value = "id") Long id) {
+        if (employeeRepository.findById(id).isPresent()) {
+            Employee employee = employeeRepository.findById(id).get();
+            employeeRepository.delete(employee);
+        }
+        return "redirect:/";
     }
 
 }
