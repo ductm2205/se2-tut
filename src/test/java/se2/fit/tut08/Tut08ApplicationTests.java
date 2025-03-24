14497bf4d56f34b398bec00f445b6721469a5dde
ductm2205 <ductranminh12342003@gmail.com>
2025-03-24 21:37:48 +0700

    init tutorial 08

---
+++ src/test/java/se2/fit/tut08/Tut08ApplicationTests.java
@@ -0,0 +1,13 @@
+package se2.fit.tut08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tut08ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
